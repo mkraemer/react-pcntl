d15b4116fa0ca7ca72996d0b88dc2d07370a0f83
Marius Krämer <marius@marius-kraemer.de>
2016-05-12 20:08:55 +0200

    Add tests covering uninstalling signal handlers

--- test/MKraemer/ReactPCNTL/PCNTLTest.php
+++ test/MKraemer/ReactPCNTL/PCNTLTest.php
@@ -4,7 +4,7 @@ namespace MKraemer\ReactPCNTL;
 use React\EventLoop\LoopInterface;
 
 function pcntl_signal($signo, $callback) {
-    PCNTLTest::$pcntl_signal_args = array($signo, $callback);
+    PCNTLTest::$pcntl_signal_args[$signo] = $callback;
 }
 function pcntl_signal_dispatch() {
     PCNTLTest::$pcntl_signal_dispatch = true;
@@ -20,7 +20,7 @@ class PCNTLTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        self::$pcntl_signal_args = null;
+        self::$pcntl_signal_args = array();
         self::$pcntl_signal_dispatch = false;
         $this->loop = $this->getMock('React\EventLoop\LoopInterface');
     }
@@ -42,9 +42,9 @@ class PCNTLTest extends \PHPUnit_Framework_TestCase
 
         $listeners = $pcntl->listeners(SIGTERM);
         $this->assertEquals(1, count($listeners));
-        $this->assertInternalType('array', self::$pcntl_signal_args);
-        $this->assertEquals(SIGTERM, self::$pcntl_signal_args[0]);
-        $this->assertSame(array($pcntl, 'emit'), self::$pcntl_signal_args[1]);
+        $this->assertEquals(1, count(self::$pcntl_signal_args));
+        $this->assertArrayHasKey(SIGTERM, self::$pcntl_signal_args);
+        $this->assertSame(array($pcntl, 'emit'), self::$pcntl_signal_args[SIGTERM]);
     }
 
     public function testInvokeCallsDispatch()
@@ -53,5 +53,60 @@ class PCNTLTest extends \PHPUnit_Framework_TestCase
         $pcntl = new PCNTL($this->loop);
         $pcntl();
         $this->assertTrue(self::$pcntl_signal_dispatch);
+    }
+
+    public function testRemoveLastListener()
+    {
+        $pcntl = new PCNTL($this->loop);
+
+        $listener = function () { };
+        $pcntl->on(SIGTERM, $listener);
+        $pcntl->removeListener(SIGTERM, $listener);
+
+        $this->assertEmpty($pcntl->listeners(SIGTERM));
+        $this->assertEquals(SIG_DFL, self::$pcntl_signal_args[SIGTERM]);
+    }
+
+    public function testRemoveNotLastListener()
+    {
+        $pcntl = new PCNTL($this->loop);
+
+        $pcntl->on(SIGTERM, function () {});
+
+        $secondListener = function () { };
+        $pcntl->on(SIGTERM, $secondListener);
+        $pcntl->removeListener(SIGTERM, $secondListener);
+
+        $this->assertNotEquals(SIG_DFL, self::$pcntl_signal_args[SIGTERM]);
+    }
+
+    public function testRemoveAllListenersOfOneSignal()
+    {
+        $pcntl = new PCNTL($this->loop);
+
+        $pcntl->on(SIGTERM, function () {});
+        $pcntl->on(SIGHUP, function () {});
+
+        $pcntl->removeAllListeners(SIGTERM);
+
+        $this->assertEmpty($pcntl->listeners(SIGTERM));
+        $this->assertNotEmpty($pcntl->listeners(SIGHUP));
+        $this->assertEquals(SIG_DFL, self::$pcntl_signal_args[SIGTERM]);
+        $this->assertSame(array($pcntl, 'emit'), self::$pcntl_signal_args[SIGHUP]);
+    }
+
+    public function testRemoveAllListenersOfAllSignals()
+    {
+        $pcntl = new PCNTL($this->loop);
+
+        $pcntl->on(SIGTERM, function () {});
+        $pcntl->on(SIGHUP, function () {});
+
+        $pcntl->removeAllListeners();
+
+        $this->assertEmpty($pcntl->listeners(SIGTERM));
+        $this->assertEquals(SIG_DFL, self::$pcntl_signal_args[SIGTERM]);
+        $this->assertEmpty($pcntl->listeners(SIGHUP));
+        $this->assertEquals(SIG_DFL, self::$pcntl_signal_args[SIGHUP]);
     }
 }
